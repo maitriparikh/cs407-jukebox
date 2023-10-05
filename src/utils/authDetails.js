959e0fa3a1a5fa1b3af47807fc6edf951c1121af
fchagua <fchagua@purdue.edu>
2023-10-05 16:29:34 -0400

    Add first change to integrate authDetails

--- src/utils/authDetails.js
+++ src/utils/authDetails.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { auth } from "./firebase";
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "../App";
 
 const AuthDetails = () => {
+    const { user, setUser } = useContext(UserContext);
     const [authUser, setAuthUser] = useState(null);
 
       /* Navigation for buttons */
@@ -12,9 +14,11 @@ const AuthDetails = () => {
     useEffect(() => {
         const listen = onAuthStateChanged(auth, (user) => {
             if (user) {
-                setAuthUser(user)
+                setAuthUser(user);
+                setUser(user.uid);
             } else {
                 setAuthUser(null);
+                setUser(null);
             }
         });
             return () => {
@@ -25,13 +29,14 @@ const AuthDetails = () => {
     const userSignOut = () => {
         signOut(auth).then(() => {
             console.log("sign out successful");
+            setUser(null);
             navigate("/");
         }).catch(error => console.log(error))
     };
 
     return (
         <div>
-            { authUser ? <><p>{`Signed In as ${authUser.email}`}</p> 
+            { authUser ? <><p>{`Signed In as ${authUser.email} and ${authUser.uid}`}</p> 
                 <button onClick={userSignOut}>Sign Out</button></>: 
                 <p>Signed Out</p>}
         </div>
