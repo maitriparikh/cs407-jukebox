fd4ce8dd5fb67a6c375421ac565dcb937c1ace54
maitriparikh <mparikh159@gmail.com>
2023-11-19 17:03:03 -0500

    music preference playlist (5 songs) saved for users that don't have spotify
    Co-authored-by: Shreya Sanapala <shrshre@users.noreply.github.com>

--- src/pages/profile_components/music_preference_quiz.js
+++ src/pages/profile_components/music_preference_quiz.js
@@ -36,19 +36,19 @@ function MusicPreferencesQuiz() {
 
     const navigate = useNavigate();
     const { user, setUser } = useContext(UserContext);
-
+    const [finalPlaylist, setFinalPlaylist] = useState([]);
     const [questions, setQuestions] = useState([
         {
           questionText: "What genre(s) of music do you like?",
           answerOptions: [
-            { answerText: "Pop", answerFlag: false},
-            { answerText: "Rap", answerFlag: false },
-            { answerText: "R&B", answerFlag: false },
-            { answerText: "Country", answerFlag: false },
-            { answerText: "EDM", answerFlag: false },
-            { answerText: "Hip Hop", answerFlag: false },
-            { answerText: "Jazz", answerFlag: false },
-            { answerText: "Classical", answerFlag: false },
+            { answerText: "Pop", answerFlag: false, playlistID: '37i9dQZF1EQncLwOalG3K7'},
+            { answerText: "Rap", answerFlag: false, playlistID: '37i9dQZF1EIgbjUtLiWmHt' },
+            { answerText: "R&B", answerFlag: false, playlistID: '37i9dQZF1EQoqCH7BwIYb7' },
+            { answerText: "Country", answerFlag: false, playlistID: '37i9dQZF1EQmPV0vrce2QZ' },
+            { answerText: "EDM", answerFlag: false, playlistID: '37i9dQZF1EIgSbGto0qEjK' },
+            { answerText: "Hip Hop", answerFlag: false, playlistID: '37i9dQZF1EQnqst5TRi17F' },
+            { answerText: "Jazz", answerFlag: false, playlistID: '37i9dQZF1EQqA6klNdJvwx' },
+            { answerText: "Classical", answerFlag: false, playlistID: '37i9dQZF1EQn1VBR3CMMWb' },
           ],
           selected: [
             
@@ -57,12 +57,12 @@ function MusicPreferencesQuiz() {
         {
           questionText: "What mood do you prefer for your songs?",
           answerOptions: [
-            { answerText: "Happy", answerFlag: false },
-            { answerText: "Romantic", answerFlag: false },
-            { answerText: "Sad", answerFlag: false },
-            { answerText: "Angry", answerFlag: false },
-            { answerText: "Depressing", answerFlag: false },
-            { answerText: "Energetic", answerFlag: false },
+            { answerText: "Happy", answerFlag: false, playlistID: '37i9dQZF1EVJSvZp5AOML2' },
+            { answerText: "Romantic", answerFlag: false, playlistID: '37i9dQZF1EVGJJ3r00UGAt' },
+            { answerText: "Sad", answerFlag: false, playlistID: '37i9dQZF1EIdChYeHNDfK5' },
+            { answerText: "Angry", answerFlag: false, playlistID: '37i9dQZF1EIgNZCaOGb0Mi' },
+            { answerText: "Depressing", answerFlag: false, playlistID: '37i9dQZF1EIfv2exTKzl3M' },
+            { answerText: "Energetic", answerFlag: false, playlistID: '37i9dQZF1EIcVD7Tg8a0MY' },
           ],
           selected: [
             
@@ -71,14 +71,15 @@ function MusicPreferencesQuiz() {
         {
           questionText: "Who's your favorite artist?",
           answerOptions: [
-            { answerText: "Taylor Swift", answerFlag: false },
-            { answerText: "Kanye West", answerFlag: false },
-            { answerText: "Lil Nas X", answerFlag: false },
-            { answerText: "Drake", answerFlag: false },
-            { answerText: "Ariana Grande", answerFlag: false },
-            { answerText: "Ice Spice", answerFlag: false },
-            { answerText: "Nicki Minaj", answerFlag: false },
-            { answerText: "BTS", answerFlag: false },
+            { answerText: "Taylor Swift", answerFlag: false, playlistID: '37i9dQZF1DX5KpP2LN299J' },
+            { answerText: "The Weeknd", answerFlag: false, playlistID: '37i9dQZF1DX6bnzK9KPvrz' },
+            { answerText: "Lana Del Rey", answerFlag: false, playlistID: '37i9dQZF1DZ06evNZVVBPG' },
+            { answerText: "Dua Lipa", answerFlag: false, playlistID: '37i9dQZF1DX3fRquEp6m8D' },
+            { answerText: "Drake", answerFlag: false, playlistID: '37i9dQZF1DX7QOv5kjbU68' },
+            { answerText: "Travis Scott", answerFlag: false, playlistID: '37i9dQZF1DZ06evO0vGf4I' },
+            { answerText: "Ariana Grande", answerFlag: false, playlistID: '37i9dQZF1DX1PfYnYcpw8w' },
+            { answerText: "Doja Cat", answerFlag: false, playlistID: '37i9dQZF1DZ06evO33svt5' },
+            { answerText: "BTS", answerFlag: false, playlistID: '37i9dQZF1DX08mhnhv6g9b' },
           ],
           selected: [
             
@@ -139,8 +140,6 @@ function MusicPreferencesQuiz() {
             // updatedQuestions[currentQuestion].selected
             updatedQuestions[currentQuestion].selected.push(answerOption.answerText)
 
-            
-            
         } else { // deselect answer and remove from selected
             const indexToRemove = updatedQuestions[currentQuestion].selected.indexOf(answerOption.answerText)
             if (indexToRemove !== -1) { // Remove answer option from selected
@@ -167,9 +166,60 @@ function MusicPreferencesQuiz() {
         await updateDoc(docRef, {
             musicPreferences: questions
         }).then(() => console.log("Document updated"));
+
+        /* CREATE DATA STRUCTURE like top5songs */
+        
+        // in selected field of question: get playlistID of each selected answer
+        
+        let playlistIDs = [];
+        questions.forEach(question => {
+            question.selected.forEach(selectedAnswerText => {
+            const matchingAnswerOption = question.answerOptions.find(answerOption => answerOption.answerText === selectedAnswerText);
+            if (matchingAnswerOption) {
+                playlistIDs.push(matchingAnswerOption.playlistID);
+            }
+            });
+        });
+        console.log("PLAYLIST IDS", playlistIDs)
+
+        // get an array with [category: all category tracks, category: all category tracks, ...]
+        let playlistDataArray = await getAllPlaylistTracks(playlistIDs);
+        console.log("playlistDataArray", playlistDataArray)
+
+        // for each playlist get item at # > tracks > items > # > track and put another data structure
+        let tracksWeWant = [];
+        playlistDataArray.forEach(playlist => {
+            playlist.tracks.items.forEach(trackInPlaylist => {
+                tracksWeWant.push(trackInPlaylist.track);
+                finalPlaylist.push(trackInPlaylist.track);
+            })
+        })
+        console.log("tracksWeWant", tracksWeWant)
+        
+        // make finalPlaylist all the combined playlist tracks
+        //setFinalPlaylist(tracksWeWant);
+        console.log("finalPlaylist", finalPlaylist);
+
+        while (finalPlaylist.length > 5) {
+            // get a random track
+            const randomIndex = getRandomIndex(finalPlaylist);
+            // remove the random track
+            finalPlaylist.splice(randomIndex, 1);
+        }
+
+        console.log("NEW finalPlaylist AFTER SPLICING", finalPlaylist);
         
         setSubmitted(true);
+
+        const docRef2 = doc(db, "users", user);
+        await updateDoc(docRef2, {
+            musicPreferencesPlaylist: finalPlaylist
+        }).then(() => console.log("Document updated"));
         
+    }
+
+    function getRandomIndex(array) {
+        return Math.floor(Math.random() * array.length);
     }
 
     const handleSubmitDialog = () => {
@@ -179,6 +229,58 @@ function MusicPreferencesQuiz() {
 
     const handleExit = () => {
         navigate("/profile")
+    }
+
+    const getAllPlaylistTracks = async (playlistIDs) => {
+        const clientId = '58126bf99c20469d8a94ca07a7dada0a';
+        const clientSecret = 'cd744e259b8b4d45a12752deaf395c11';
+
+        // Step 1: Obtain an access token using the Client Credentials Flow
+        const tokenResponse = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(`${clientId}:${clientSecret}`),
+        },
+        body: 'grant_type=client_credentials',
+        });
+
+        const tokenData = await tokenResponse.json();
+
+        if (!tokenData.access_token) {
+        console.error('Failed to obtain access token');
+        return null;
+        }
+
+        // const playlistIDs = [POP, RAP, R&B, COUNTRY, EDM, HIP HOP, JAZZ, CLASSICAL]; // playlist IDs acc to answer choices
+        //const playlistIDs = ['37i9dQZF1EQncLwOalG3K7', '37i9dQZF1EIgbjUtLiWmHt', '37i9dQZF1EQoqCH7BwIYb7', '37i9dQZF1EQmPV0vrce2QZ', '37i9dQZF1EIgSbGto0qEjK', '37i9dQZF1EQnqst5TRi17F', '37i9dQZF1EQqA6klNdJvwx', '37i9dQZF1EQn1VBR3CMMWb']; 
+        const playlistDataPromises = playlistIDs.map(async (playlistID) => {
+            const apiUrl = `https://api.spotify.com/v1/playlists/${playlistID}`;
+            const res = await fetch(apiUrl, {
+            headers: {
+                'Authorization': `Bearer ${tokenData.access_token}`,
+                'Content-Type': 'application/json',
+            },
+            method: 'GET',
+            });
+
+                return res.json();
+        });
+
+        try {
+            // Wait for all playlist data requests to complete
+            const playlistDataArray = await Promise.all(playlistDataPromises);
+    
+            // Combine all playlist data into a single array
+            let combinedPlaylistArray = playlistDataArray.map(playlistData => playlistData);
+    
+            // Return the combined array
+            return combinedPlaylistArray;
+        } catch (error) {
+            console.error("Error fetching playlist data:", error);
+            return []; // Return an empty array in case of error
+        }
+
     }
 
     return (
